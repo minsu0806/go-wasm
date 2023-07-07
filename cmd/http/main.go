c9d53c7a51d78330be5119f7962c5d123eb5916e
minsu0806 <minsu0806@gmail.com>
2023-07-08 00:11:57 +0900

    test

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
 	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("../../assets"))))
-	fmt.Println("asdf")
 }
